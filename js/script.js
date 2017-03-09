1f9bbae397246ed3952afe98e54542525e28fb2f
Alperen Talaslıoğlu <alperentalaslioglu@Alperens-MacBook-Pro.local>
2017-03-09 23:40:55 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
 
+    var clockHours = new Date().getHours();
+
+    if(clockHours < 12 ){
+         $("#msg").html('<img src="img/morning.png"> Good Morning.');
+
+    }else if(clockHours > 12 && clockHours < 20){
+        $("#msg").html('<img src="img/afternoon.png"> Good Afternoon.');
+
+    }else{
+        $("#msg").html('<img src="img/night.png"> Good Night.');
+    }
+
+
 
 });
