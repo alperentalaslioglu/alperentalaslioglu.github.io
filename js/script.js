dc17aa255b0f3901fcf6aabdfc919b4b55fc5532
Alperen <alperentalaslioglu@users.noreply.github.com>
2017-03-12 23:19:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     var clockHours = new Date().getHours();
 
-    if(clockHours < 12 ){
+    if(clockHours < 12 && clockHours > 6 ){
          $("#msg").html('<img src="img/morning.png"> Good Morning.');
 
     }else if(clockHours > 12 && clockHours < 20){
